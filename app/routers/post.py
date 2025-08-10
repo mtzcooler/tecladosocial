2fd432180273f5bcfcb83fe054410ad4ea39da8f
Matheus <mtzcooler@gmail.com>
2025-08-10 19:29:17 +0100

    Put together comments and posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,10 +1,17 @@
 from http import HTTPStatus
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import List
 
-from app.schemas.post import PostCreate, PostRead
+from app.schemas.post import (
+    PostCreate,
+    PostRead,
+    CommentCreate,
+    CommentRead,
+    PostWithComments,
+)
 
 post_table = {}
+comment_table = {}
 
 router = APIRouter(
     prefix="/posts",
@@ -12,8 +19,12 @@ router = APIRouter(
 )
 
 
+async def find_post(post_id: int) -> dict:
+    return post_table.get(post_id, {})
+
+
 @router.post("", name="Create post", status_code=HTTPStatus.CREATED)
-async def create(post: PostCreate) -> PostRead:
+async def create_post(post: PostCreate) -> PostRead:
     data = post.dict()
     last_record_id = len(post_table)
     new_post = {**data, "id": last_record_id}
@@ -22,5 +33,40 @@ async def create(post: PostCreate) -> PostRead:
 
 
 @router.get("", name="List posts", status_code=HTTPStatus.OK)
-async def index() -> List[PostRead]:
+async def list_posts() -> List[PostRead]:
     return list(post_table.values())
+
+
+@router.post(
+    "/{post_id}/comments", name="Create comment", status_code=HTTPStatus.CREATED
+)
+async def create_comment(post_id: int, comment: CommentCreate) -> CommentRead:
+    post = await find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Post not found")
+
+    data = comment.dict()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "post_id": post_id, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return CommentRead(**new_comment)
+
+
+@router.get("/{post_id}/comments", name="List comments", status_code=HTTPStatus.OK)
+async def list_comments(post_id: int) -> List[CommentRead]:
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@router.get("/{post_id}", name="Get post with comments", status_code=HTTPStatus.OK)
+async def read_post_with_comments(post_id: int) -> PostWithComments:
+    post = await find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Post not found")
+
+    posts_with_comments = {
+        "post": post,
+        "comments": await list_comments(post_id),
+    }
+    return PostWithComments(**posts_with_comments)
