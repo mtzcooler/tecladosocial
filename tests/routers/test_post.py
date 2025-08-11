790b3cb4b8829738f1b94f0b00983e3e5a408ebc
Matheus <mtzcooler@gmail.com>
2025-08-11 17:05:55 +0100

    Add tests for posts

--- tests/routers/test_post.py
+++ tests/routers/test_post.py
@@ -10,5 +10,24 @@ async def test_create_post(async_client: AsyncClient):
         "/posts",
         json={"body": body},
     )
+
     assert response.status_code == HTTPStatus.CREATED
     assert {"id": 0, "body": "Test Post"}.items() <= response.json().items()
+
+
+@pytest.mark.anyio
+async def test_create_post_missing_data(async_client: AsyncClient):
+    response = await async_client.post(
+        "/posts",
+        json={},
+    )
+
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.anyio
+async def test_list_all_posts(async_client: AsyncClient, created_post: dict):
+    response = await async_client.get("/posts")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == [created_post]
